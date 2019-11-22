64d66ae6bf420c8fc8260dd20bae832b4de87a7b
Ian Patrick Badtrousers <badt@veritas.icu>
2019-11-22 23:57:50 +0200

    better screen, install.sh updated

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ cd config
 
 cp .gitconfig $HOME
 cp .tmux.conf $HOME
+tmux source-file $HOME/.tmux.conf
 cp .vimrc $HOME
 mkdir -p $HOME/.vim && cp -r .vim $HOME
-mkdir -p $HOME/.config && cp -r fish $HOME/.config
 
 sudo apt install fish
 mkdir -p /usr/local/bin
 sudo ln -s /usr/bin/fish /usr/local/bin
+curl -L https://get.oh-my.fish | fish
+mkdir -p $HOME/.config && cp -r fish $HOME/.config
